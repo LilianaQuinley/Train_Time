8f9dbefe7903d5f5edd0ba7cd0ec4063f49b15c8
liliana Quinley <lilicuellar99@gmail.com>
2018-10-04 00:06:34 -0600

    Push to Data Base

--- trainJava.js
+++ trainJava.js
@@ -18,23 +18,12 @@ $(document).ready(function () {
 
     var db = firebase.database();
 
-    
-
     console.log(db);
 
     // // This callback keeps the page updated when a value changes in firebase.
     db.ref("Train").on("value",function (snapshot){
       console.log("Everything back?")
       console.log(snapshot.val());
-      // if (snapshot.child("Name").exist() && 
-      // snapshot.child("Destination").exist() && 
-      // snapshot.child ("First_Train").exist() && 
-      // snapshot.child("Frequency")){
-      //   Name = snapshot.val().Name;
-      //   Destination = snapshot.val().Destination;
-      //   First_Train = parseInt (snapshot.val().First_Train);
-      //   Frequency = parseInt(snapshot.val().Frequency);
-      // }
         
     });
     // // We are now inside our .on function...
@@ -49,10 +38,6 @@ $(document).ready(function () {
         var First_Train = $("#firstTrain").val().trim();
         var Frequency = $("#frequency").val().trim();
 
-        console.log(Name);
-        console.log(Destination);
-        console.log(First_Train);
-        console.log(Frequency);
 
         db.ref("Train").push({
           Name: Name,
@@ -61,64 +46,29 @@ $(document).ready(function () {
           Frequency: Frequency,
         });
 
+              // Clears all of the text-boxes
+      $("#trainName").val("");
+      $("#destination").val("");
+      $("#firstTrain").val("");
+      $("#frequency").val("");
+
    });
 
-   
-   
+   db.ref("Train").on('child_added', function (childSnapshot, prevChildKey){
+    console.log(childSnapshot.val());
 
+    var trains = childSnapshot.val();
+    var row = $('<tr>');
+    row.append('<td>' + trains.Name + '</td>');
+    row.append('<td>' + trains.Destination + '</td>');
+    row.append('<td>' + trains.Frequency + '</td>');
+    row.append('<td />');
+    row.append('<td />');
+    $('#results').append(row);
+
+   });
 
 });
-
-
-
-// db.ref("Train").push({
-//   Name: "Peter the Great Train",
-//   Destination: "St Petersburg, Russia",
-//   First_Train: "21:00",
-//   Frequency: 270,
-// });
-// db.ref("Train").push({
-//   Name: "Versailles Train",
-//   Destination: "Versailles, France",
-//   First_Train: "18:50",
-//   Frequency: 450,
-// });
-// db.ref("Train").push({
-//   Name: "Sistine Chapel",
-//   Destination: "Vatican City, Rome",
-//   First_Train: "03:30",
-//   Frequency: 540,
-// });
-// db.ref("Train").push({
-//   Name: "Notre Dame Train",
-//   Destination: "Paris, France",
-//   First_Train: "10:40",
-//   Frequency: 210,
-// });
-// db.ref("Train").push({
-//   Name: "The Colosseum Train",
-//   Destination: "Roma, Italy",
-//   First_Train: "21:00",
-//   Frequency: 210,
-// });
-// db.ref("Train").push({
-//   Name: "The Vatican St Peter's Train",
-//   Destination: "Vatican City, Rome",
-//   First_Train: "15:45",
-//   Frequency: 540,
-// });
-// db.ref("Train").push({
-//   Name: "Hagia Sophia Train",
-//   Destination: "Istanbul, Turkey",
-//   First_Train: "08:15",
-//   Frequency: 360,
-// });
-// db.ref("Train").push({
-//   Name: "Santorini Train",
-//   Destination: "Santorini, Greece",
-//   First_Train: "17:15",
-//   Frequency: 500,
-// });
 
 
 
